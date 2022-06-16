59b6945df3c7b10466bd0f7acb33ecc08d55b3bd
JiangBo <1006758852@qq.com>
2022-06-16 08:20:03 +0000

    增加iproute命令

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -4,7 +4,7 @@ echo "init sh script"
 
 yum update -y && yum upgrade -y
 yum install -y git glibc-devel.x86_64 glibc-headers.x86_64 gcc \
-    vim curl
+    vim curl iproute
 
 curl https://sh.rustup.rs -sSf | sh -s -- -y --profile complete
 
