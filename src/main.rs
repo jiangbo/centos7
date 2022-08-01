5febdf6ef78e0e66d9a89ae4b98381e3df6e639a
JiangBo <1006758852@qq.com>
2022-08-01 07:27:23 +0000

    feat:还原项目

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,3 @@
-use anyhow::{Context, Result};
-use crossterm::{terminal, ExecutableCommand};
-use tui::backend::{Backend, CrosstermBackend};
-
-mod app;
-
-pub fn main() -> Result<()> {
-    terminal::enable_raw_mode()?;
-
-    let mut backend = CrosstermBackend::new(std::io::stdout());
-    backend
-        .execute(terminal::EnterAlternateScreen)?
-        .execute(terminal::Clear(terminal::ClearType::All))?
-        .hide_cursor()?;
-    let mut terminal = tui::Terminal::new(backend)?;
-
-    app::App::new().run(&mut terminal)?;
-
-    terminal::disable_raw_mode()?;
-    terminal
-        .backend_mut()
-        .execute(terminal::Clear(terminal::ClearType::All))?
-        .execute(terminal::LeaveAlternateScreen)?
-        .show_cursor()
-        .context("reset terminal error")
+fn main() {
+    println!("hello, world!");
 }
